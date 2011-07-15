af2b1bdd0a3d7405a99e015d0def0decea0055a6
Niall Parker <stuff@npengineering.ca>
2011-07-15 11:48:36 -0700

    Initial commit to git

---
+++ ledTest/led.c
@@ -0,0 +1,32 @@
+/* Blink LED example */
+
+#include <msp430g2231.h>
+
+/** Delay function. **/
+void delay(unsigned int d) {
+  int i;
+  for (i = 0; i<d; i++) {
+        nop();
+  }
+}
+
+int main(void) {
+	unsigned char p1reg=0;
+
+  WDTCTL = WDTPW | WDTHOLD;
+  P1DIR = 0xF7;
+  P1OUT = 0x08;
+  P1REN = 0x08;
+
+  for (;;) {
+    P1OUT = 0x08;
+    delay(0x8fff);
+    p1reg = 0x09;
+    if (P1IN & 0x08)
+	    p1reg |= 0x40;
+    P1OUT = p1reg;
+    delay(0x8fff);
+  }
+  return 0;
+}
+
